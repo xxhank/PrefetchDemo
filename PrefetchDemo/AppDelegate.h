5577be7af9e09f6ee3945d3a24a8b4cdc01b028f
王超9 <wangchao9@le.com>
2017-07-29 10:38:27 +0800

    Initial Commit

---
+++ PrefetchDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PrefetchDemo
+//
+//  Created by wangchao9 on 2017/7/29.
+//  Copyright © 2017年 Beijing Ninety Culture Co.ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PrefetchDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PrefetchDemo
+//
+//  Created by wangchao9 on 2017/7/29.
+//  Copyright © 2017年 Beijing Ninety Culture Co.ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
