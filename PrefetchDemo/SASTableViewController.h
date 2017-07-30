da334af736fe570d1faa359fb5bfc444fcf7547f
王超9 <wangchao9@le.com>
2017-07-30 10:57:15 +0800

    实现拉下刷新,上拉更多和预加载

--- PrefetchDemo/SASTableViewController.h
+++ PrefetchDemo/SASTableViewController.h
@@ -6,42 +6,116 @@
 //  Copyright © 2017年 Ying Shi Da Quan. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
-// #import "DQRefresh.h"
 #import "SASViewController.h"
 #import "NTYTableViewProxy.h"
-
-typedef void (^RefreshingBlock)();
 
 @interface SASTableViewController : SASViewController
 @property (nonatomic, strong) IBOutlet UITableView *tableView;
 @property (nonatomic, strong) NTYTableViewProxy    *proxy;
 
-//@property (nonatomic, strong) DQRefreshHeader           *header;
-//@property (strong, nonatomic) MJRefreshAutoNormalFooter *footer;
-// 预加载block
-@property (nonatomic, copy) RefreshingBlock preloadBlock;
-@property (nonatomic, assign) NSInteger     preloadPosition;
-
-// 设置 header 下拉刷新
-- (void)setRefreshHeader:(BOOL)addControl refreshBlock:(RefreshingBlock)block;
-// 设置 footer 上拉刷新
-- (void)setRefreshFooter:(BOOL)addControl refreshBlock:(RefreshingBlock)block;
-/**
- *  设置底部刷新控件的状态
- *
- *  @param hasMoreData 是否还有更多的数据
- */
-- (void)setRefreshFooterState:(BOOL)hasMoreData;
-
-
-- (void)endRefreshFooterState:(BOOL)hasMoreData;
-- (void)endRefreshHeader;
-
-
-// 加载更多
-
 #pragma mark - support subclassing
 - (UITableView*)buildTableView;
 - (NTYTableViewProxy*)buildTableViewProxy;
+
+#pragma mark - header/footer
+/**
+ *  是否允许下拉刷新控件
+ */
+@property (nonatomic, assign) BOOL refreshHeaderEnabled;
+/**
+ *  是否允许上拉更多控件
+ */
+@property (nonatomic, assign) BOOL refreshFooterEnabled;
+
+/**
+ *  通知下拉刷新完成
+ *
+ *  @param hasMoreData 是否还有更多的数据
+ */
+- (void)loadNewDataFinished:(BOOL)hasMoreData;
+
+/**
+ *  通知上拉加载更多完成
+ *
+ *  @note 预加载也是上拉加载更多的一种形式
+ *  @param hasMoreData 是否还有更多的数据
+ */
+- (void)loadMoreDataFinished:(BOOL)hasMoreData;
+
+#pragma mark - header/footer subclassing
+/**
+ *  加载新数据, 子类实现具体的行为
+ *  @note 子类重载时需要调用 [super loadNewData];
+ *  @code
+   - (void)loadNewData {
+   [super loadNewData];
+
+   /// other code
+   }
+ *  @endcode
+ */
+- (void)loadNewData;
+
+/**
+ *  加载更多数据, 子类实现具体的行为
+ *  @note 子类重载时需要调用 [super loadNewData];
+ *  @param prefetch 是否又预加载触发
+ *  @code
+   - (void)loadMoreData:(BOOL)prefetch {
+    /// check can load more
+
+    [super loadMoreData:prefetch];
+
+    /// load more
+   }
+ *  @endcode
+ */
+- (void)loadMoreData:(BOOL)prefetch;
+
+#pragma mark - prefetch
+/**
+ *  预加载数据的行数阈值
+ */
+@property (nonatomic, assign) NSUInteger prefetchRowDistance;
+/**
+ *  预加载数据的偏移量阈值, 是当前一屏内容高度的比例
+ */
+@property (nonatomic, assign) CGFloat prefetchOffsetPrecent;
+
+/**
+ *  检测scrollView的偏移量,并预加载数据
+ *
+ *  @param scrollView 需要预加载的view
+    @code
+   - (void)scrollViewDidScroll:(UIScrollView*)scrollView {
+      [self prefetchByCheckOffset:scrollView];
+   }
+    @endcode
+ */
+- (void)prefetchByCheckOffset:(UIScrollView*)scrollView;
+
+/**
+ *  检测当前显示的cell的indexPaths,并预加载数据
+ *
+ *  @param indexPath 将要显示的cell的位置
+ *  @code
+   - (void)tableView:(UITableView*)tableView willDisplayCell:(UITableViewCell*)cell forRowAtIndexPath:(NSIndexPath*)indexPath {
+    /// other code
+   [self prefetchByCheckIndexPath:indexPath];
+   };
+ *  @endcode
+ */
+- (void)prefetchByCheckIndexPath:(NSIndexPath*)indexPath;
+/**
+ *  停止预加载, 需要子类实现该行为
+ *  @note 子类重载时需要调用 [super cancelPrefetch]
+ *  @code
+   - (void)cancelPrefetch{
+    /// cancel request code
+
+    [super cancelPrefetch];
+   }
+   @endcode
+ */
+- (void)cancelPrefetch;
 @end
--- PrefetchDemo/SASTableViewController.m
+++ PrefetchDemo/SASTableViewController.m
@@ -11,7 +11,9 @@
 #import "NTYTableView.h"
 
 @interface SASTableViewController ()
-
+@property (nonatomic, assign) BOOL             prefetching;
+@property (nonatomic, strong) MJRefreshHeader *refreshHeader;
+@property (nonatomic, strong) MJRefreshFooter *refreshFooter;
 @end
 
 @implementation SASTableViewController
@@ -22,6 +24,8 @@ DQMEMD_TRACE_CHILDREN(self.tableView, self.proxy);
 #pragma mark - Override
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.prefetchOffsetPrecent = 0.3;
+    self.prefetchRowDistance   = 2;
 
     self.tableView = self.tableView?:[self buildTableView];
     self.proxy     = [self buildTableViewProxy];
@@ -45,44 +49,166 @@ DQMEMD_TRACE_CHILDREN(self.tableView, self.proxy);
     return [NTYTableViewProxy proxy:self.tableView];
 }
 
-#pragma mark - 下拉刷新， 上拉刷新
-//
-- (void)setRefreshHeader:(BOOL)add refreshBlock:(RefreshingBlock)block {
+#pragma mark - header/footer
+- (BOOL)refreshHeaderEnabled {return self.tableView.mj_header != nil;}
+- (void)setRefreshHeaderEnabled:(BOOL)refreshHeaderEnabled {
+    if (refreshHeaderEnabled) {
+        [self addRefreshHeader];
+    } else {
+        [self removeRefreshHeader];
+    }
+}
+
+- (BOOL)refreshFooterEnabled {return self.tableView.mj_footer != nil;}
+- (void)setRefreshFooterEnabled:(BOOL)refreshFooterEnabled {
+    if (refreshFooterEnabled) {
+        [self addRefreshFooter];
+    } else {
+        [self removeRefreshFooter];
+    }
+}
+
+#pragma mark - header/footer  private
+- (void)addRefreshHeader {
+    if (!self.refreshHeader) {
+        @weakify(self);
+        self.refreshHeader = [[MJRefreshGifHeader headerWithRefreshingBlock:^{
+            @strongify(self);
+            [self loadNewData];
+        }] sas_decorate:^(__kindof MJRefreshGifHeader*_Nonnull header) {
+            header.lastUpdatedTimeLabel.hidden = YES;
+            header.stateLabel.hidden = YES;
+
+            const NSUInteger idleImagesCount = 13;
+            NSMutableArray *idleImages = [NSMutableArray arrayWithCapacity:idleImagesCount];
+            for (int i = 1; i <= idleImagesCount; i++) {
+                [idleImages addObject:[UIImage imageNamed:STRING(@"下拉%d", i)]];
+            }
+            [header setImages:idleImages forState:MJRefreshStateIdle];
+
+            const NSUInteger refreshingImagesCount = 19;
+            NSMutableArray *refreshingImages = [NSMutableArray arrayWithCapacity:refreshingImagesCount];
+            for (int i = 1; i <= refreshingImagesCount; i++) {
+                [refreshingImages addObject:[UIImage imageNamed:STRING(@"转动%d", i)]];
+            }
+            [header setImages:refreshingImages forState:MJRefreshStateRefreshing];
+        }];
+    }
+
+    self.tableView.mj_header = self.refreshHeader;
+}
+
+- (void)removeRefreshHeader {
     [self.tableView.mj_header removeFromSuperview];
     self.tableView.mj_header = nil;
-
-    if (add) {
-        MJRefreshHeader *header = [[MJRefreshHeader alloc] init];
-        self.tableView.mj_header = header;
-        header.refreshingBlock   = ^() {
-            if (block) {
-                block();
-            }
-        };
-    }
 }
 
-- (void)setRefreshFooter:(BOOL)add refreshBlock:(RefreshingBlock)block {
+- (void)addRefreshFooter {
+    if (!self.refreshFooter) {
+        @weakify(self);
+        self.refreshFooter = [[MJRefreshBackNormalFooter footerWithRefreshingBlock:^{
+            @strongify(self);
+            if (!self.prefetching) {
+                [self loadMoreData:NO];
+            }
+        }] sas_decorate:^(__kindof MJRefreshBackNormalFooter*_Nonnull footer) {
+            footer.activityIndicatorViewStyle = UIActivityIndicatorViewStyleGray;
+            footer.stateLabel.textColor = COLOR_HEXA(0x000000, 0.5);
+            footer.stateLabel.font = [UIFont systemFontOfSize:14];
+
+            // 初始化文字
+            [footer setTitle:@"上拉加载更多 ..." forState:MJRefreshStateIdle];
+            [footer setTitle:@"松开手指刷新 ..." forState:MJRefreshStatePulling];
+            [footer setTitle:@"正在加载 ..." forState:MJRefreshStateRefreshing];
+            [footer setTitle:@"没有更多了" forState:MJRefreshStateNoMoreData];
+        }];
+    }
+
+    self.tableView.mj_footer = self.refreshFooter;
+}
+
+- (void)removeRefreshFooter {
     [self.tableView.mj_footer removeFromSuperview];
     self.tableView.mj_footer = nil;
+}
 
-    if (add) {
-        MJRefreshFooter *footer = [[MJRefreshFooter alloc] init];
-        self.tableView.mj_footer = footer;
-        footer.refreshingBlock   = block;
+#pragma mark - header/footer subclassing
+- (void)loadNewData {
+    NSLogInfo(@"");
+    [self cancelPrefetch];
+}
+
+- (void)loadMoreData:(BOOL)prefetch {
+    NSLogInfo(@"prefetch: %@", @(prefetch));
+    if (prefetch) {
+        self.tableView.mj_footer.state = MJRefreshStateRefreshing;
     }
 }
 
-- (void)setRefreshFooterState:(BOOL)hasMoreData {
+- (void)loadNewDataFinished:(BOOL)hasMoreData {
+    [self.tableView.mj_header endRefreshing];
     self.tableView.mj_footer.state = hasMoreData? MJRefreshStateIdle:MJRefreshStateNoMoreData;
 }
 
-- (void)endRefreshFooterState:(BOOL)hasMoreData {
-    [self.tableView.mj_footer endRefreshing];
+- (void)loadMoreDataFinished:(BOOL)hasMoreData {
+    self.prefetching               = NO;
     self.tableView.mj_footer.state = hasMoreData? MJRefreshStateIdle:MJRefreshStateNoMoreData;
 }
-- (void)endRefreshHeader {
-    [self.tableView.mj_header endRefreshing];
+
+#pragma mark - prefetch
+- (void)prefetchByCheckOffset:(UIScrollView*)scrollView  {
+    if (self.prefetching) {
+        return;
+    }
+
+    CGFloat contentOffset   = RECT_BOTTOM(scrollView.bounds);
+    CGFloat contentHeight   = scrollView.contentSize.height;
+    CGFloat pageHeight      = RECT_HEGIHT(scrollView.frame);
+    CGFloat contentDistance = contentHeight - contentOffset;
+
+    BOOL    canPrefetch = contentDistance < pageHeight * self.prefetchOffsetPrecent;
+    BOOL    notOverflow = contentDistance > 10; /// 避免向上拉动tableView引起的越界
+    if (notOverflow && canPrefetch) {
+        NSLogInfo(@"%@", @[@(contentOffset), @(contentHeight)]);
+        NSLog(@"start prefetch");
+        self.prefetching = YES;
+        [self loadMoreData:YES];
+    }
+}
+
+- (void)prefetchByCheckIndexPath:(NSIndexPath*)indexPath {
+    if (self.prefetching) {
+        return;
+    }
+
+    NSUInteger totalRows           = 0;
+    NSUInteger totalRowForCurrent  = 0;
+    NSUInteger currentSectionIndex = indexPath.section;
+    NSUInteger sectionCount        = self.tableView.numberOfSections;
+    for (NSUInteger index = 0; index < sectionCount; index++) {
+        NSUInteger rowCount = [self.tableView numberOfRowsInSection:index];
+        totalRows += rowCount;
+        if (index < currentSectionIndex) {
+            totalRowForCurrent += rowCount;
+        }
+    }
+
+    totalRowForCurrent += indexPath.row + 1;
+    NSInteger rowDistance = totalRows - totalRowForCurrent;
+
+    NSLogInfo(@"%@", @[@(totalRowForCurrent), @(totalRows)]);
+    BOOL canPrefetch = rowDistance < self.prefetchRowDistance + 1;
+    BOOL notOverflow = rowDistance >= 0;      /// 避免向上拉动tableView引起的越界
+    if (notOverflow && canPrefetch) {
+        NSLog(@"start prefetch");
+        self.prefetching = YES;
+        [self loadMoreData:YES];
+    }
+}
+
+- (void)cancelPrefetch {
+    self.prefetching = NO;
+    NSLogInfo(@"cancel prefetch");
 }
 
 @end
